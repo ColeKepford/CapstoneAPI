72842543bac798b7ccce0f935b9a8ea642d72ce0
Andre <andre.colaris98@live.ca>
2021-02-24 16:47:51 -0700

    Added URL for adding a client

--- src/main/java/com/HailMaryAPI/HailMary/Clients/ClientController.java
+++ src/main/java/com/HailMaryAPI/HailMary/Clients/ClientController.java
@@ -4,10 +4,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 
 import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDate;
 import java.util.List;
 
 @RestController
-@RequestMapping
+@RequestMapping("/clients")
 @CrossOrigin(origins="http://localhost:3000")
 public class ClientController {
     private final ClientService clientService;
@@ -22,13 +23,27 @@ public class ClientController {
         return clientService.getAllClients();
     }
 
-    @GetMapping("/clients/{id}")
+    @GetMapping("/{id}")
     public Client getClient(@PathVariable("id")Integer id) {
         return clientService.getClientById(id);
     }
 
-    @PostMapping
-    public void addNewClient(@RequestBody Client client) {
+    @GetMapping("/addClient/{email}&{first_name}&{last_name}&{dob}&{phone_number}&{street_address}&{prov}&{country}&{postal_code}&{password}")
+    public void addNewClient(@PathVariable("email") String email,
+                            @PathVariable("first_name") String first_name,
+                            @PathVariable("last_name") String last_name,
+                            @PathVariable("dob") String dob,
+                            @PathVariable("phone_number") String phone_number,
+                            @PathVariable("street_address") String street_address,
+                            @PathVariable("prov") String province,
+                            @PathVariable("country") String country,
+                            @PathVariable("postal_code") String postal_code,
+                            @PathVariable("password") String password) {
+        //Dont know how HTML formats date
+        LocalDate birth = LocalDate.parse(dob);
+        Long phone = Long.parseLong(phone_number);
+        Client client = new Client(email, first_name, last_name, birth, phone, street_address, 
+                                    province, country, postal_code, password);
         clientService.addNewClient(client);
     }
 

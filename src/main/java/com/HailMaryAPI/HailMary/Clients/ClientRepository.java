a541bd97411b702b228aeab947144d12b3abad2c
Andre <andre.colaris98@live.ca>
2021-03-17 18:19:35 -0600

    Update now works

--- src/main/java/com/HailMaryAPI/HailMary/Clients/ClientRepository.java
+++ src/main/java/com/HailMaryAPI/HailMary/Clients/ClientRepository.java
@@ -1,10 +1,13 @@
 package com.HailMaryAPI.HailMary.Clients;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
+
+import javax.transaction.Transactional;
 
 @Repository
 public interface ClientRepository extends JpaRepository<Client, Integer> {
@@ -15,10 +18,10 @@ public interface ClientRepository extends JpaRepository<Client, Integer> {
     @Query("Select c FROM Client c WHERE c.email = ?1 AND c.password = ?2")
     Optional<Client> findClient(String email, String password);
 
-    @Query("UPDATE Client SET email = ?1, first_name = ?2, last_name = ?3, date_of_birth = ?4, phone_number = ?5, " +
-            "street_address = ?6, province = ?7, country = ?8, postal_code = ?9, password = ?10 " +
-            "WHERE client_id = ?11")
-    Optional<Client> updateClient(String email, String first_name, String last_name, String date_of_birth,
+    @Transactional
+    @Modifying
+    @Query("UPDATE Client SET email = ?1, first_name = ?2, last_name = ?3, date_of_birth = ?4, phone_number = ?5, street_address = ?6, province = ?7, country = ?8, postal_code = ?9, password = ?10 WHERE client_id = ?11")
+    void updateClient(String email, String first_name, String last_name, String date_of_birth,
                                     Long phone_number, String street_address, String province, String country,
                                     String postal_code, String password, int id);
 }
